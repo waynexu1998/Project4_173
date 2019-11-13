a9366e04a79b57445b31c77c65cd76660f334224
waynexu1998 <cobrawaynexu@gmail.com>
2019-11-13 15:26:22 -0500

    Initial Commit

---
+++ Project4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project4
+//
+//  Created by Jiangyong Xu on 11/13/19.
+//  Copyright © 2019 Jiangyong Xu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
