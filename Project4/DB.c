87fe36cf14aa8e064b92cf4f08047cf3aa100a83
waynexu1998 <cobrawaynexu@gmail.com>
2019-11-14 00:01:56 -0500

    Init

---
+++ Project4/DB.c
@@ -0,0 +1,59 @@
+//
+//  hashTable.c
+//  Project4
+//
+//  Created by Jiangyong Xu on 11/13/19.
+//  Copyright © 2019 Jiangyong Xu. All rights reserved.
+//
+
+#include "DB.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int hash(int key){
+    return key%SIZE;
+}
+
+typedef struct CSG{   //CSG structure
+    char course[5];
+    int studentID;
+    char grade[2];
+    CSG *next;
+}CSG;
+
+
+
+void insert(int key, CSG tuple){
+    int index = hash(key);
+    if(CSG_DB[index] == NULL){
+        *CSG_DB[index] = tuple;
+    }else{
+        if(CSG_DB[index]->next != NULL){
+            *CSG_DB[index]->next->next = tuple;
+        }else{
+            *CSG_DB[index]->next = tuple;
+        }
+    }
+}
+
+
+struct CSG *lookup(char *course, char *studentID, char *grade){      //hash table lookkup
+    int id = atoi(studentID);
+    CSG* outputCSG[SIZE];
+    int arrayCount = 0;
+    
+    if (course[0] == '*') {                                             //course left as '*'
+        for(int i=0; i<SIZE; i++){
+            if (CSG_DB[i]->studentID == id && CSG_DB[i]->grade == grade) {
+                outputCSG[arrayCount++] = CSG_DB[i];
+            }
+        }
+    }else if (studentID[0] == '*'){                                     //studentID left as '*'
+        for (int i=0; i<SIZE; i++) {
+            if (CSG_DB[i]->course == course && CSG_DB[i]->grade ==grade) {
+                outputCSG[arrayCount++] = CSG_DB[i];
+            }
+        }
+    }
+    return *outputCSG;
+}
